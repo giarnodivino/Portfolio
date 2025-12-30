80b7e6d437b62bf5238e4e4c0475530695a1a18a
Giarno Divino <giar@Giarnos-MacBook-Air.local>
2025-12-30 19:25:35 +0800

    changes in design and responsiveness

--- src/assets/svg-icons.jsx
+++ src/assets/svg-icons.jsx
@@ -52,4 +52,23 @@ const Django = ({color, link}) => {
     )
 }
 
-export {JavaScript, Python, HTML, CSS, Bulma, Django, ReactJS, Tailwind};
+const LinkedIn = ({color, link}) => {
+    return (
+        <a href={`${link}`} target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={`${color}` } viewBox="0 0 256 256" className="opacity-30 hover:opacity-80 duration-300 ease-in-out"><path d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"></path></svg></a>
+    )
+}
+
+const Github = ({color, link}) => {
+    return (
+        <a href={`${link}`} target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={`${color}`} viewBox="0 0 256 256" className="opacity-30 hover:opacity-80 duration-300 ease-in-out"><path d="M208.31,75.68A59.78,59.78,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58.14,58.14,0,0,0,208.31,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.72,41.72,0,0,1,200,104Z"></path></svg></a>
+    )
+}
+
+const Mail = ({color, link}) => {
+    return (
+        <a href={`${link}`} target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill={`${color}`} viewBox="0 0 256 256" className="opacity-30 hover:opacity-80 duration-300 ease-in-out"><path d="M231.87,114l-168-95.89A16,16,0,0,0,40.92,37.34L71.55,128,40.92,218.67A16,16,0,0,0,56,240a16.15,16.15,0,0,0,7.93-2.1l167.92-96.05a16,16,0,0,0,.05-27.89ZM56,224a.56.56,0,0,0,0-.12L85.74,136H144a8,8,0,0,0,0-16H85.74L56.06,32.16A.46.46,0,0,0,56,32l168,95.83Z"></path></svg></a>
+    )
+}
+
+
+export {JavaScript, Python, HTML, CSS, Bulma, Django, ReactJS, Tailwind, LinkedIn, Github, Mail};
