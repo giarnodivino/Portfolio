2853c73f945899bd7b95e372e707d1fd67abc1d0
Giarno Divino <giar@Giarnos-MacBook-Air.local>
2025-12-30 20:31:06 +0800

    fixing deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/newport/',
+  base: '/Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
